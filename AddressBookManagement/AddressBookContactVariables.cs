eccb050ab73d21663b6605141415d67d48bfe00b
abh224272 <abh224272@gmail.com>
2020-12-17 17:30:25 +0530

    [Abhishek Mishra] Add: Created A method to accept values and create new Address Book

---
+++ AddressBookManagement/AddressBookContactVariables.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookManagement
+{
+    class AddressBookContactVariables
+    {
+        private String fName;
+        private String lName;
+        private String Address;
+        private String city;
+        private String state;
+        private String email;
+        private int zip;
+        private long phone;
+
+        public String GetfName()
+        {
+            return fName;
+        }
+
+        public void SetfName(String fName)
+        {
+            this.fName = fName;
+        }
+
+        public String SetlName()
+        {
+            return lName;
+        }
+
+        public void SetlName(String lName)
+        {
+            this.lName = lName;
+        }
+
+        public String GetAddress()
+        {
+            return Address;
+        }
+
+        public void SetAddress(String address)
+        {
+            Address = address;
+        }
+
+        public String GetCity()
+        {
+            return city;
+        }
+
+        public void SetCity(String city)
+        {
+            this.city = city;
+        }
+
+        public String GetState()
+        {
+            return state;
+        }
+
+        public void SetState(String state)
+        {
+            this.state = state;
+        }
+
+        public String GetEmail()
+        {
+            return email;
+        }
+
+        public void SetEmail(String email)
+        {
+            this.email = email;
+        }
+
+        public int GetZip()
+        {
+            return zip;
+        }
+
+        public void SetZip(int zip)
+        {
+            this.zip = zip;
+        }
+
+        public long GetPhone()
+        {
+            return phone;
+        }
+
+        public void SetPhone(long phone)
+        {
+            this.phone = phone;
+        }
+
+        override
+        public String ToString()
+        {
+            return "AddressBookContactVariables{" +
+                    "fName='" + fName + '\'' +
+                    ", lName='" + lName + '\'' +
+                    ", Address='" + Address + '\'' +
+                    ", city='" + city + '\'' +
+                    ", state='" + state + '\'' +
+                    ", email=" + email + '\'' +
+                    ",zip=" + zip +
+                    ", phone=" + phone +
+                    '}';
+        }
+    }
+}
---
+++ AddressBookManagement/AddressBookOperations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookManagement
+{
+    class AddressBookOperations
+    {
+        public AddressBookContactVariables NewAddressBook()
+        {
+            AddressBookContactVariables addressBookContactVariables = new AddressBookContactVariables();
+            Console.WriteLine("Enter the First Name");
+            string fName = Console.ReadLine();
+            Console.WriteLine("Enter the Last Name");
+            string lName = Console.ReadLine();
+            Console.WriteLine("Enter The Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter the city");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter the state");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter the Email Id of Contact");
+            string email = Console.ReadLine();
+            Console.WriteLine("Enter the zip Code");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Phone number");
+            long phone = Convert.ToInt64(Console.ReadLine());
+            addressBookContactVariables.SetfName(fName);
+            addressBookContactVariables.SetlName(lName);
+            addressBookContactVariables.SetAddress(address);
+            addressBookContactVariables.SetCity(city);
+            addressBookContactVariables.SetState(state);
+            addressBookContactVariables.SetEmail(email);
+            addressBookContactVariables.SetZip(zip);
+            addressBookContactVariables.SetPhone(phone);
+            return addressBookContactVariables;
+        }
+
+    }
+}
